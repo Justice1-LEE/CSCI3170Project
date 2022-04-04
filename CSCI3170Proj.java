f429e52938f3026852cbc8e9ef648a1f10a2cd82
李航吉 <lihangji@lihangjideMacBook-Pro.local>
2022-04-04 14:19:23 +0800

    asdfas

--- CSCI3170Proj.java
+++ CSCI3170Proj.java
@@ -467,7 +467,7 @@ class CSCI3170Proj {
             Statement stmt = con.createStatement();
             String query = "SELECT C.callnum, P.copynum, C.name, D.cname, R.checkout, R.return_date " +
                     "FROM car C, copy P, produce D, rent R " +
-                    "WHERE C.callnum = P.callnum AND C.callnum = D.callnum AND P.copynum = R.copynum AND R.uid = '"
+                    "WHERE C.callnum = P.callnum AND C.callnum = D.callnum AND P.copynum = R.copynum AND R.callnum = C.callnum AND R.uid = '"
                     + userID +
                     "' ORDER by R.checkout DESC";
             ResultSet result = stmt.executeQuery(query);
