bc77beb8b07d12b9ef69faa7d9a7abeb29c12b3b
李航吉 <lihangji@lihangjideMacBook-Pro.local>
2022-04-04 15:26:58 +0800

    asdfdsf

--- CSCI3170Proj.java
+++ CSCI3170Proj.java
@@ -553,8 +553,7 @@ class CSCI3170Proj {
             Statement stmt = con.createStatement();
             String query = "SELECT uid, callnum, copynum, checkout " +
                     "FROM rent " +
-                    "WHERE return_date is NULL AND Date(checkout) between Date_format('" + startDate
-                    + "', '%Y-%m-%d)'  and Date_format('" + endDate + "', '%Y-%m-%d)";
+                    "where DATE(checkout) between DATE_FORMAT(STR_TO_DATE('01/01/2022','%d/%m/%Y'), '%Y-%m-%d') and DATE_FORMAT(STR_TO_DATE('04/04/2022','%d/%m/%Y'), '%Y-%m-%d')";
             ResultSet result = stmt.executeQuery(query);
             while (result.next()) {
                 String uid = result.getString("uid");
