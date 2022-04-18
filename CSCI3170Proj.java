ec85c773886de0c4c6a881fbb53cd9df02787f14
JI Yi <80081125+yiji0@users.noreply.github.com>
2022-04-18 10:45:07 +0800

    Update CSCI3170Proj.java

--- CSCI3170Proj.java
+++ CSCI3170Proj.java
@@ -571,9 +571,11 @@ class CSCI3170Proj {
                 stmt.executeUpdate(qry);
                 return true;
             }
-        } catch (SQLException e) {
-            System.out.println("!!!!!!!!!!!!!");
-            // System.out.println("[\u001B[31mError\u001B[0m]: \u001B[31mNo\u001B[0m Matching car copy found.");
+        // } catch (SQLException e) {
+        //     System.out.println("[\u001B[31mError\u001B[0m]: \u001B[31mNo\u001B[0m Matching car copy found.");
+        //     return false;
+        } catch (e) {
+            System.out.println(e);
             return false;
         }
     }
